82b97f7f9d7b1e2c5484c204debc7a14cc6e1291
HeladodePistacho <th_sola@hotmail.com>
2020-11-01 23:00:13 +0100

    aDDED Bicho

---
+++ Assets/SimpleAnimat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAnimat : MonoBehaviour
+{
+    Animator anim;
+    bool change = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine(SwapFrame());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SwapFrame()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(SoundManager.Instance.beat.clip.length);
+            anim.SetBool("Swap", change);
+            change = !change;
+        }
+    }
+}
