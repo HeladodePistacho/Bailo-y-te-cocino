49fede3d5d51e05ce1c341cfb3dc24ae0cf2691e
fran_botttos <fran.rgimenez@gmail.com>
2020-11-01 21:23:16 +0100

    Load scene implemented in MainMenu

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -15,7 +15,7 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0)
         {
             manager.CheckIngredient();
         }
